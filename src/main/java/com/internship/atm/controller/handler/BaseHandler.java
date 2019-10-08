7ff5e807cbc81ca3b274c41d5472e92b4b265d04
iddqd2d <iddqd2d@gmail.com>
2019-10-08 16:54:04 +0300

    some fix

--- src/main/java/com/internship/atm/controller/handler/BaseHandler.java
+++ src/main/java/com/internship/atm/controller/handler/BaseHandler.java
@@ -1,6 +1,7 @@
 package com.internship.atm.controller.handler;
 
 import com.internship.atm.controller.handler.exception.BankCardNotFoundExeption;
+import com.internship.atm.controller.handler.exception.NotEnoughMoneyExeption;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
@@ -11,17 +12,24 @@ public class BaseHandler {
 
     @ExceptionHandler(BankCardNotFoundExeption.class)
     public BaseResponse handlerNotFoundException(BankCardNotFoundExeption ex) {
-        return BaseResponse.buildErrorResponse(ex.getMessage());
+        return BaseResponse.buildErrorResponse(404, ex.getMessage());
+    }
+
+    @ExceptionHandler(BankCardNotFoundExeption.class)
+    public BaseResponse handlerNotEnoughMoneyExeption(NotEnoughMoneyExeption ex) {
+        return BaseResponse.buildErrorResponse(404, ex.getMessage());
     }
 
     @Setter
     @Getter
     private static class BaseResponse {
+        private int errorCode = 200;
         private boolean success = true;
         private String errorMessage;
 
-        static BaseResponse buildErrorResponse(String errorMessage) {
+        static BaseResponse buildErrorResponse(int errorCode, String errorMessage) {
             BaseResponse baseResponse = new BaseResponse();
+            baseResponse.setErrorCode(errorCode);
             baseResponse.setErrorMessage(errorMessage);
             baseResponse.setSuccess(Boolean.FALSE);
             return baseResponse;
